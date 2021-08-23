b0fb5aafb7e09ae128594e1d4f05423da35fad87
Alejo Percudani <alejopercudani@outlook.com>
2021-08-23 16:49:13 -0300

    conexion a db

---
+++ UI/static/js/db.js
@@ -0,0 +1,38 @@
+var express = require('express'); // npm i express
+var app = express();
+
+app.get('/', function (req, res) {
+       
+    var sql = require("mssql"); // npm i mssql
+
+    // config for your database
+    var config = {
+        user: 'sa',
+        password: 'mypassword',
+        server: 'localhost', 
+        database: 'SchoolDB' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from articulos', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
